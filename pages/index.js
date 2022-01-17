7a4a23d6217cc6f8feb2bce458a04f7c6537c215
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2022-01-16 18:28:26 -0600

    added template pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,4 +15,6 @@ export default function Home() {
       </main>
     </div>
   );
-}
+};
+
+export default Home;
